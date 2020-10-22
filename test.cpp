c3e2640e4921b1d2bb86078c19be9c5af1a577c3
slagu002 <slagu002@ucr.edu>
2020-10-21 21:15:54 -0700

    adding test.cpp along with sample c-count.h

--- test.cpp
+++ test.cpp
@@ -1,49 +1,34 @@
 #include "c-echo.h"
+#include "c-count.h"
 
 #include "gtest/gtest.h"
 
 TEST(EchoTest, HelloWorld) {
-	char* test_val[3]; test_val[0] = "./c-echo";
+	char* test_val[3]; test_val[0] = "./c-echo-count";
 	test_val[1] = "hello";
 	test_val[2] = "world";
 	EXPECT_EQ("hello world", echo(3,test_val));
 }
 
 TEST(EchoTest, EmptyString) {
-	char* test_val[1]; test_val[0] = "./c-echo";
+	char* test_val[1]; test_val[0] = "./c-echo-count";
 	EXPECT_EQ("", echo(1,test_val));
 }
 
-TEST(EchoTest, NewLine) {
-	char* test_val[2]; test_val[0] = "./c-echo";
-	test_val[1] = "\n";
-	EXPECT_EQ("\n", echo(2, test_val));
+TEST(CountTest, HelloWorld) {
+    std::string test_str = "hello world";
+    EXPECT_EQ(2, count(test_str));
 }
 
-TEST(EchoText, Tab) {
-	char* test_val[2]; test_val[0] = "./c-echo";
-	test_val[1] = "\t";
-	EXPECT_EQ("\t", echo(2, test_val));
+TEST(CountTest, EmptyString) {
+    std::string test_str = "";
+    EXPECT_EQ(0, count(test_str));
 }
 
-TEST(EchoTest, BackSlash) {
-	char* test_val[2]; test_val[0] = "./c-echo";
-	test_val[1] = "\\";
-	EXPECT_EQ("\\", echo(2, test_val));
-}
-
-TEST(EchoTest, Space) {
-	char* test_val[2]; test_val[0] = "./c-echo";
-	test_val[1] = " ";
-	EXPECT_EQ(" " , echo(2, test_val));
-}
-
-TEST(EchoTest, IntChar) {
-	char* test_val[2]; test_val[0] = "./c-echo";
-	test_val[1] = "0123";
-	EXPECT_EQ("0123", echo(2, test_val));
-}
-	
+TEST(CountTest, ManySpaces) {
+    std::string test_str = "   this   string has     weird   spacing";
+    EXPECT_EQ(5, count(test_str));
+}	
 
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
